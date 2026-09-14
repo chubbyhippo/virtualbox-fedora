2e400c95b1186e0961aba13cac9d58ce809f29a0
chubbyhippo <mkmsacc@outlook.com>
2026-09-14 09:17:03 +0700

    Install and configure Mise during initialization.

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ sudo dnf install -y openssh-server
 sudo systemctl enable --now sshd
 sudo firewall-cmd --permanent --add-service=ssh
 sudo firewall-cmd --reload
+
+[ -x ~/.local/bin/mise ] || curl -fsSL https://mise.run | sh
+mkdir -p ~/.config/mise
+[ -f ~/.config/mise/config.toml ] || curl -fsSL https://raw.githubusercontent.com/chubbyhippo/virtualbox-fedora/refs/heads/main/mise.toml -o ~/.config/mise/config.toml
