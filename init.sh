eb206bcb1d675e83df73e0f552e010b41a4f4e7d
chubbyhippo <mkmsacc@outlook.com>
2026-09-14 14:49:23 +0700

    Install development tools group during initialization.

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 sudo dnf upgrade -y
 
+sudo dnf install -y @development-tools
+
 sudo dnf install -y openssh-server
 sudo systemctl enable --now sshd
 sudo firewall-cmd --permanent --add-service=ssh
